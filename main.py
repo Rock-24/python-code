d014ec6c9cf83f681d04a1e69feb0c9156d3747b
cipher <fychacker@gmail.com>
2023-02-11 10:32:22 -0800

    first commit

---
+++ main.py
@@ -0,0 +1,29 @@
+# Python is a popular programming language.
+
+# Python can be used on a server to create web applications.
+
+#printing a line
+print('hello world')
+#print variable
+x = 5
+y = "John"
+print(x)
+print(y)
+# python list
+thislist = ["apple", "banana", "cherry"]
+
+thislist[1:3] = ["watermelon"]
+
+print(thislist)
+
+#loop in python
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  print(x)
+  #function in python
+def my_function(fname):
+  print(fname + " Refsnes")
+
+my_function("Emil")
+my_function("Tobias")
+my_function("Linus")
